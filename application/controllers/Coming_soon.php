27a2262218997b9f8f85a4efe5fef059abb49547
AlvaroMrJack <alvaro.12tobar@gmail.com>
2017-07-02 23:51:18 -0400

    vista próximamente | Modificaciones para adaptar el template

---
+++ application/controllers/Coming_soon.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Coming_soon extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('proximamente/proximamente');
+	}
+
+}
+
+/* End of file Coming_soon.php */
+/* Location: ./application/controllers/Coming_soon.php */
